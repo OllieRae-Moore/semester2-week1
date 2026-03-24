84b1b3a713ecfe3e403197959d07530f1343405b
OllieRae-Moore <fwjh0382@leeds.ac.uk>
2026-03-24 21:35:03 +0000

    q3 atmpt1

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,15 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+.mode columns
+.header on
+SELECT DepartmentName,
+COUNT(EnrolmentId) AS TotalEnrolments
+FROM Department
+--join course on department.departmentid = course.departmentid
+--join enrolment on course.courseid = enrolment.courseid
+left join course on department.departmentid = course.departmentid
+left join enrolment on course.courseid = enrolment.courseid 
+--added left join just in case some departments dont have enrolments or courses
+group by departmentname;
+
