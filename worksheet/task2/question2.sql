c6a8a8be09fe8bf3e9bc645637b2047741d29ab8
OllieRae-Moore <fwjh0382@leeds.ac.uk>
2026-03-24 20:26:32 +0000

    q2done

--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,3 +1,9 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
+.mode columns
+.headers on
+SELECT StudentId, FirstName, LastName, DateOfBirth
+From student
+ORDER BY DateofBirth DESC
+LIMIT 5;
