277b3ab8384dc2c159e76fb7521a239fa934b9b6
OllieRae-Moore <fwjh0382@leeds.ac.uk>
2026-03-24 20:26:00 +0000

    q1done

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,8 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+.mode columns
+.headers  on
+SELECT StudentId, FirstName, LastName, EnrolmentYear
+From student 
+WHERE EnrolmentYear = 2022;
